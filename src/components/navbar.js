47e9d54d59f4b0ce0cb9e62e603291b4e127a93c
zhengjianliu <comezheng@gmail.com>
2021-02-10 20:27:27 -0500

    done

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react'
 
 export default class Navbar extends Component{
+    state={
+        open:false,
+    }
+    clickHandler = () =>{
+        this.setState({open:!this.state.open})
+    }
     render(){
         return(
             <div className="Navabr">
-                <ul className="hamburger">
+                <ul className={this.state.open?"hamburger active":"hamburger"} onClick={this.clickHandler}>
                     <li></li>
                     <li></li>
                     <li></li>
                 </ul>
-                <ul className="items">
+                <ul className={this.state.open?"items active":"items"}>
                     <li>Home</li>
                     <li>About</li>
                     <li>blogs</li>
